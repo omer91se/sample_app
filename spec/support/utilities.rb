2664fccee983c62772b25d953cae63c18f7a994d
omer91se <omer.segal@fiverr.com>
2019-08-14 11:02:41 +0300

    Finish user edit, update, index, and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,3 +1,5 @@
+require 'spec_helper'
+
 def full_title(page_title)
   base_title = "Ruby on Rails Tutorial Sample App"
   if page_title.empty?
@@ -5,5 +7,14 @@ def full_title(page_title)
   else
     "#{base_title} | #{page_title}"
   end
-
 end
+
+def sign_in_back(user)
+  visit signin_path
+  fill_in "Email", with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  # Sign in when not using Capybara as well.
+  cookies[:remember_token] = user.remember_token
+end
+
